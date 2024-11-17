a5209fbe476f7d912079b7d0c498bbafed452000
artemD7 <artem.dubovetskyi@weizmann.ac.il>
2024-11-17 22:22:35 +0200

    updated file

--- day03/day03.py
+++ day03/day03.py
@@ -1,5 +1,17 @@
 print('hello5')
-width = 10
-height = 20
+# width = 10
+# height = 20
+# area = width * height
+# print(area)
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--width', help='Width in pixels', required = True, type=int)
+parser.add_argument('--height', help='Height in pixels in pixels', required = True, type=int)
+
+args = parser.parse_args() 
+width = args.width
+height = args.height
+
 area = width * height
 print(area)
