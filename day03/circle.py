277139a28aad75a5f548c8091fb0e099356f8279
artemD7 <artem.dubovetskyi@weizmann.ac.il>
2024-11-18 14:59:49 +0200

    new files

--- day03/circle.py
+++ day03/circle.py
@@ -1,4 +1,8 @@
-r = float(input('Please enter the radius: '))
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--radius',help='radius in units', required =True, type=int)
+arg = parser.parse_args()
+r = arg.radius
 pi = 3.14
-print('The area of the circle is', pi*r**2 )
-print('The circumference of the circle is', 2*pi*r)
+print(f'The area of the circle is {pi*r**2:.1f}')
+print(f'The circumference of the circle is {2*pi*r:.1f}')
