0687a9ba7a3acb4d8053fe723fd742930d2e0525
artemD7 <artem.dubovetskyi@weizmann.ac.il>
2024-11-29 20:43:03 +0200

    new files

--- day04/The guess_number_game.py
+++ day04/The guess_number_game.py
@@ -1,5 +1,5 @@
 import random
-
+#let's define the function which reacts on user entering "n"  
 def user_press_n():
     while True:
         gamer_check_2 = str(input('Do you want to play a new game? Enter "yes" or "no". ' ))
@@ -11,7 +11,8 @@ def user_press_n():
             print('Please type "yes" or "no"')
             continue
     return   
-
+# this function works when the user plays second or more game in a row and contains
+# user_press_n function
 def second_game_and_more():
     gamer_check = str(input('Do you want to play again? Enter "yes" or "no". ' ))
     if gamer_check == 'yes':
@@ -47,7 +48,8 @@ def second_game_and_more():
         print("Please type the right answer")
         return
 
-
+# the function is reponsible for the very first game of the user in the sequence 
+# of games and contains user_press_n function
 def first_game(game_counter):
     computer_guess = random.randrange(1,21)
     counter_guess = 0
@@ -77,13 +79,14 @@ def first_game(game_counter):
             continue
     print('The number of guessing attempts:', counter_guess)
     return game_counter
-
+# this is the main function which operates with second_game_and_more and 
+# first_game function
 def main(game_counter):
     while True:
         if  game_counter > 0:
             second_game_and_more()
         else:
             game_counter = first_game(game_counter)
-
+# here we do the functions call
 game_counter = 0
 main(game_counter)
